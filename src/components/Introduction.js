87aa44e6af0c0b3db31916bcd8df073b53a4e30e
Caryin <yeecaryin@gmail.com>
2022-04-10 16:12:30 +0800

    refactor introduction

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -9,10 +9,13 @@ import {
   Button,
   Link,
   useColorModeValue,
+  Stack,
+  Center,
+  Spacer,
 } from '@chakra-ui/react';
 
 //icon
-import { Icon } from '@chakra-ui/icons';
+import { EmailIcon, Icon } from '@chakra-ui/icons';
 import { LinkIcon } from '@chakra-ui/icons';
 import { FiGithub } from 'react-icons/fi';
 
@@ -20,7 +23,7 @@ const Introduction = () => {
   const fontColor = useColorModeValue('gray.700', 'rgb(221, 217, 216)');
 
   return (
-    <VStack alignItems='flex-start'>
+    <VStack my={6} alignItems='flex-start'>
       <Heading size='sm' color='pink.500'>
         INTRODUCTION
       </Heading>
@@ -35,20 +38,35 @@ const Introduction = () => {
         of software development. I am a competitive person and I love the
         challenge of learning new technologies.
       </Text>
-      <HStack py={12}>
-        <Link isExternal href='https://resume.io/r/FIVYyvn1m'>
-          <Button size='lg' colorScheme='telegram' variant='outline' mx={2}>
-            <LinkIcon mr='10px' />
-            RESUME
-          </Button>
-        </Link>
+      <HStack
+        py={8}
+        alignItems='center'
+        // bg="red.500"
+        w='full'
+        justifyContent={['space-around', 'flex-start']}
+        gap={4}
+      >
+        <Button
+          as={Link}
+          isExternal
+          colorScheme='telegram'
+          variant='outline'
+          href='https://resume.io/r/FIVYyvn1m'
+        >
+          <LinkIcon mr='10px' />
+          RESUME
+        </Button>
 
-        <Link isExternal href='https://github.com/Caryin'>
-          <Button size='lg' colorScheme='purple' variant='outline' mx={2}>
-            <Icon as={FiGithub} w={5} h={5} mr='10px' />
-            GITHUB
-          </Button>
-        </Link>
+        <Button
+          as={Link}
+          colorScheme='purple'
+          variant='outline'
+          isExternal
+          href='https://github.com/Caryin'
+        >
+          <Icon as={FiGithub} w={5} h={5} mr='10px' />
+          GITHUB
+        </Button>
       </HStack>
     </VStack>
   );
