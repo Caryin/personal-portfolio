8f5264367790ea807a0e87861d20a27d614f4071
Caryin <yeecaryin@gmail.com>
2022-04-10 16:11:08 +0800

    refactor contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 //chakraUI
 import {
-  Container,
   Heading,
-  Stack,
   HStack,
   Button,
   Link,
   Icon,
   useColorModeValue,
+  VStack,
 } from '@chakra-ui/react';
 
 //chakraUI-icon
@@ -20,31 +19,43 @@ const Contact = () => {
   const fontColor = useColorModeValue('gray.700', 'rgb(221, 217, 216)');
 
   return (
-    <Container maxW='container.lg' id='contact' p={10}>
+    <VStack id='contact' pb={32}>
       <Heading size='md' color='pink.500' textAlign='center' pt={20}>
         CONTACT
       </Heading>
       <Heading color={fontColor} size='lg' textAlign='center' pt={3}>
-        Ways To Contact Me
+        Contact Me
       </Heading>
 
-      <Stack py={[7, 10, 20]} alignItems='center'>
-        <HStack spacing='45px'>
-          <Link isExternal href='mailto:yeecaryin@gmail.com'>
-            <Button size='lg' colorScheme='purple' variant='outline'>
-              <EmailIcon mr={3} />
-              E-Mail
-            </Button>
-          </Link>
-          <Link isExternal href='https://www.linkedin.com/in/caryinyee/'>
-            <Button size='lg' colorScheme='telegram' variant='outline'>
-              <Icon as={FaLinkedin} mr='10px' />
-              LinkedIn
-            </Button>
-          </Link>
-        </HStack>
-      </Stack>
-    </Container>
+      <HStack
+        py={[7, 10, 20]}
+        alignItems='center'
+        // w="full"
+        gap={[2, 4]}
+        justifyContent={['space-around', 'center']}
+      >
+        <Button
+          as={Link}
+          isExternal
+          href='mailto:yeecaryin@gmail.com'
+          colorScheme='purple'
+          variant='outline'
+        >
+          <EmailIcon mr='10px' />
+          E-Mail
+        </Button>
+        <Button
+          as={Link}
+          isExternal
+          href='https://www.linkedin.com/in/caryinyee/'
+          colorScheme='telegram'
+          variant='outline'
+        >
+          <Icon as={FaLinkedin} mr='10px' />
+          LinkedIn
+        </Button>
+      </HStack>
+    </VStack>
   );
 };
 
