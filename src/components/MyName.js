a8de6293a148391e49101235364c8e0ab3bdf946
Caryin <yeecaryin@gmail.com>
2022-04-10 16:13:05 +0800

    refactor myname

--- src/components/MyName.js
+++ src/components/MyName.js
@@ -2,29 +2,26 @@ import React from 'react';
 import ProfilePhoto from '../assets/ProfilePhoto.jpg';
 
 //chakraUI
-import { VStack, Heading, Avatar, useColorModeValue } from '@chakra-ui/react';
+import {
+  VStack,
+  Heading,
+  Avatar,
+  useColorModeValue,
+  Text,
+  Image,
+} from '@chakra-ui/react';
 
 const MyName = () => {
   const fontColor = useColorModeValue('gray.700', 'rgb(221, 217, 216)');
   return (
-    <VStack
-      w='full'
-      h='full'
-      alignItems='flex-start'
-      pl={{ sm: 5, md: '60px' }}
-    >
+    <VStack py={6} pr={[0, 28]}>
       <Heading size='sm' color={fontColor}>
         Hi, I'm
       </Heading>
       <Heading size='2xl' color={fontColor} pb={3} mt={0}>
         Caryin
       </Heading>
-      <Avatar
-        boxSize='3xs'
-        src={ProfilePhoto}
-        name='Caryin'
-        alignItems='flex-start'
-      ></Avatar>
+      <Avatar boxSize={['180px', '250px']} src={ProfilePhoto} name='Caryin' />
     </VStack>
   );
 };
